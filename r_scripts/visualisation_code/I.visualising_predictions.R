c673c79b905c39e9a1a24533e46142514eecd1c9
franciscapowell <84057903+franciscapowell@users.noreply.github.com>
2022-05-20 13:43:36 +1000

    Add files via upload

---
+++ r_scripts/visualisation_code/I.visualising_predictions.R
@@ -0,0 +1,129 @@
+# Dependencies
+if(!require(caret)){
+  install.packages('caret')
+}
+
+if(!require(dplyr)){
+  install.packages('dplyr')
+}
+
+if(!require(gbm)){
+  install.packages('gbm')
+}
+
+if(!require(mgcv)){
+  install.packages('mgcv')
+}
+
+if(!require(purrr)){
+  install.packages('purrr')
+}
+
+if(!require(randomForestSRC)){
+  install.packages('randomForestSRC')
+}
+
+if(!require(ggplot2)){
+  install.packages('ggplot2')
+}
+
+if(!require(Hmsc)){
+  install.packages('Hmsc')
+}
+
+
+library(parallel)
+
+setwd("C:/.../MEE_feature_ensemble")
+
+#### Testing the stacked prediction algorithm's performance for out-of-sample prediction. Here
+# we run three cross validation folds for each dataset ####
+source('functions/stacking_function.R')
+
+
+#### Visualising results ####
+
+load("outputs/test_predictions/all_predictions.rda")
+
+predictions <- all_predictions[all_predictions$Method != "BASE", ]
+
+
+My_Theme = theme(
+  axis.title.x = element_text(size = 20),
+  axis.title.y = element_text(size = 20),
+  axis.text.x = element_text(size = 15),
+  axis.text.y = element_text(size = 15))
+
+predictions$Method <- as.character(predictions$Method)
+predictions$Method[predictions$Method == "GBM_PR"] <- "GBM-PR"
+predictions$Method[predictions$Method == "GBM_DR"] <- "GBM-DR"
+predictions$Method[predictions$Method == "NULL_ENS"] <- "NULL-ENS"
+
+ggplot(predictions, aes(x = Recall_adj, y = Method)) + 
+  geom_boxplot(aes(fill = Method)) + 
+  geom_vline(xintercept = 0, linetype = 'dashed') +
+  theme_bw() + theme(legend.position = 'none') +
+  xlab('Improvement in Recall over GLM-BASE') + 
+  ylab('Model') +
+  My_Theme
+
+
+ggplot(predictions, aes(x = Precision_adj, y = Method)) + 
+  geom_boxplot(aes(fill = Method)) + 
+  geom_vline(xintercept = 0, linetype = 'dashed') +
+  theme_bw() + theme(legend.position = 'none') +
+  xlab('Improvement in Precision over GLM-BASE')  + 
+  ylab('Model') +
+  My_Theme
+
+
+ggplot(predictions, aes(x = F1_adj, y = Method)) + 
+  geom_boxplot(aes(fill = Method)) + 
+  geom_vline(xintercept = 0, linetype = 'dashed') +
+  theme_bw() + theme(legend.position = 'none') +
+  xlab('Improvement in F1 over GLM-BASE')  + 
+  ylab('Model') +
+  My_Theme
+
+
+jpeg(file = "figures/300_dpi/testing_F1_boxplot.jpeg", units="mm", width=568, height=315, res=300)
+ggplot(predictions, aes(x = F1_adj, y = Method)) + 
+  geom_boxplot(aes(fill = Method)) + 
+  geom_vline(xintercept = 0, linetype = 'dashed') +
+  theme_bw() + theme(legend.position = 'none') +
+  xlab('Improvement in F1 over GLM-BASE')  + 
+  ylab('Model') +
+  My_Theme
+dev.off()
+
+
+# Summary of species prevalence categories
+all_predictions %>%
+  dplyr::group_by(Method) %>%
+  dplyr::mutate(Total_species = length(unique(Species))) %>%
+  dplyr::select(Total_species) %>% dplyr::distinct() %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(Prop = Total_species / sum(Total_species))
+
+# Summary of meaningful changes in F1
+predictions %>%
+  dplyr::group_by(Method) %>%
+  dplyr::summarise(Total_better = length(which(F1_adj > 0.02)),
+                   Total_nochange = length(which(F1_adj <= 0.02 & F1_adj >= -0.02)),
+                   Total_worse = length(which(F1_adj < -0.02)))
+
+# Summary of meaningful changes in Precision
+predictions %>%
+  dplyr::group_by(Method) %>%
+  dplyr::summarise(Total_better = length(which(Precision_adj > 0.02)),
+                   Total_nochange = length(which(Precision_adj <= 0.02 & Precision_adj >= -0.02)),
+                   Total_worse = length(which(Precision_adj < -0.02)))
+
+# Summary of meaningful changes in Recall
+predictions %>%
+  dplyr::group_by(Method) %>%
+  dplyr::summarise(Total_better = length(which(Recall_adj > 0.02)),
+                   Total_nochange = length(which(Recall_adj <= 0.02 & Recall_adj >= -0.02)),
+                   Total_worse = length(which(Recall_adj < -0.02)))
+
+
